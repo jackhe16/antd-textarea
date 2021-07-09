18c088d85ba1182187fa802bee9cb3e9d90c156b
jackhe16 <jackhe16@126.com>
2021-07-09 09:51:35 +0800

    Input.TextArea

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from 'react';
+import { Input } from 'antd';
+import "antd/dist/antd.css";
 
-function App() {
+const App: FC = () => {
+  const [text, setText] = useState('text');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input.TextArea value={text} />
     </div>
   );
 }
